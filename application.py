b212f24131178d53af8bdb55d42db9427e7fa61e
Hemanth Vasireddy <hemanthvasireddy@outlook.com>
2023-10-14 12:23:32 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,24 +4,6 @@ from Block import Block
 from Transaction import Transaction
 from Payment import Pay
 
-# Generate six pairs of public and private keys for users
-keys = [ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1) for _ in range(6)]
-
-# Initial user with 100 coins
-genesis_user = keys[0]
-genesis_user_balance = 100
-
-# Create a genesis transaction
-genesis_transaction = Transaction(genesis_user.verifying_key.to_string().hex(), keys[0].verifying_key.to_string().hex(), genesis_user_balance)
-genesis_transaction.sign_transaction(genesis_user.to_string().hex())
-
-# Create a genesis block
-genesis_block = Block([genesis_transaction], "0")
-
-# Create a blockchain with the genesis block
-blockchain = [genesis_block]
-
-# Add more blocks with transactions
 
 
 # Perform transactions between keys
